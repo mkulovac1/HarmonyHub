60a732183f28e86f5f0768cb1d41ade9c2d96101
Merim Kulovac <92273186+mkulovac1@users.noreply.github.com>
2023-08-18 13:25:22 +0200

    search is now working - searching by artist or song

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,43 @@
-const Search = () => (
-  <div>Search</div>
-);
+import { useSelector } from 'react-redux';
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCore';
+import { useParams } from 'react-router-dom';
+
+const Search = () => { 
+    
+    const { searchTerm } = useParams(); // This is the search term from the URL
+
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+    const { data, isFetching, error } = useGetSongsBySearchQuery(searchTerm);
+
+    if(isFetching) return <Loader title="Searching..." />
+
+    if(error)
+        return <Error title="Oops! Something went wrong" message="Please try again later" />
+
+    const songs = data?.tracks?.hits?.map((song) => song.track)
+
+    return (
+        <div className="flex flex-col">
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+                Showing results form <span className="font-black">{searchTerm}</span>
+            </h2>
+
+            <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+                {songs?.map((song, i) => (
+                    <SongCard 
+                        key={song.key} 
+                        song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
+                        i={i}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
 
 export default Search;
